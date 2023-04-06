4fb11a5f661ff40f808891c64f4cd3a31b6bb32c
Gabriel Marchetti dee Souza <gbielmarch@gmail.com>
2023-04-06 08:56:32 -0300

    Terminei

---
+++ Exercício-backend3/Login-nomeEsenha/Program.cs
@@ -0,0 +1,58 @@
+﻿// Entrada
+
+Console.WriteLine($"Qual é o seu nome de usúario");
+string user = Console.ReadLine();
+
+Console.WriteLine($"Qual é a sua senha");
+string pass = Console.ReadLine();
+while(user == pass)
+{
+    Console.WriteLine($"Usuário e a senha não podem ser iguais. Digite uma senha ou usúario válido");
+    
+    Console.WriteLine($"Digite o usuário");
+    user = Console.ReadLine();
+    
+    Console.WriteLine($"Digite a senha ");
+    pass = Console.ReadLine();    
+}
+
+Console.WriteLine($"Tela de login montada");
+
+Console.WriteLine($"Novamente, cofirme nos o seu username");
+string userR1 =Console.ReadLine();
+
+Console.WriteLine($"Agora sua senha");
+string passR1 = Console.ReadLine();
+
+while(user != userR1 || pass != passR1)
+{
+    if( user != userR1 && pass != passR1)
+    {
+        Console.WriteLine($"Nome e senha inválido, digite novamente");
+
+        Console.WriteLine($"Nome");
+        userR1 = Console.ReadLine();
+
+        Console.WriteLine($"Senha");
+        passR1 = Console.ReadLine();        
+    }
+
+    else if (user != userR1 || pass != passR1)
+    {
+        Console.WriteLine($"Nome ou senha inválido, digite novamente");
+
+        Console.WriteLine($"Nome");
+        userR1 = Console.ReadLine();
+
+        Console.WriteLine($"Senha");
+        passR1 = Console.ReadLine(); 
+    }
+}
+
+Console.WriteLine($"Sua conta foi logada, bem vindo {userR1}");
+
+
+
+
+
+
---
+++ Exercício-backend3/nota/Program.cs
@@ -0,0 +1,47 @@
+﻿// Entrada
+Console.WriteLine($"Qual foi sua nota. Lembrando digite uma nota de 0 a 10");
+int nota = int.Parse(Console.ReadLine());
+
+while(nota < 0 || nota > 10)
+{
+    Console.WriteLine($"Nota inválida, digita uma nota válida");
+    nota = int.Parse(Console.ReadLine());
+        
+}
+
+switch(nota)
+{
+case 0:
+    Console.WriteLine($" Terrível,melhore!");
+    break;
+case 1:
+    Console.WriteLine($" Terrível,melhore!");
+    break;
+case 2:
+    Console.WriteLine($" Terrível,melhore!");
+    break;
+case 3:
+    Console.WriteLine($" Terrível,melhore!");
+    break;
+case 4:
+    Console.WriteLine($" Terrível,melhore!");
+    break;
+case 5:
+    Console.WriteLine($" Você pode melhorar!");
+    break;
+case 6:
+    Console.WriteLine($" Voce pode melhorar!");
+    break;
+case 7:
+    Console.WriteLine($" Parabéns, mas você ainda pode melhorar!");
+    break;
+case 8:
+    Console.WriteLine($" Parabéns, mas você ainda pode melhorar!");
+    break;
+case 9:
+    Console.WriteLine($" Muito bem!!!");
+    break;
+case 10:
+    Console.WriteLine($" MUITO BEMMMM, PArabéns, nota máxima para vc ;)");
+    break;
+}
