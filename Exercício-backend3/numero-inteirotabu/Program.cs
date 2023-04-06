31bee627c319d359e448396be881e97cc5a680d9
Gabriel Marchetti dee Souza <gbielmarch@gmail.com>
2023-04-06 09:50:44 -0300

    Terminando

---
+++ Exercício-backend3/numero-inteirotabu/Program.cs
@@ -0,0 +1,12 @@
+﻿// Entrada
+
+Console.WriteLine($"Digite um número inteiro");
+int n1 = int.Parse(Console.ReadLine());
+int resultado;
+
+for (int i = 1; i <= 10 ; i++)
+{
+    resultado = n1*i;
+    Console.WriteLine($"{n1} X {i} = {resultado}");
+}
+
